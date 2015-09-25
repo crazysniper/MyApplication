64f7c916d23ed0ddb667909a0d7d429fe0316437
crazysniper <xiaofeng@XiaofengGedeMacBook-Pro.local>
2015-09-25 16:07:44 +0800

    22

---
+++ MyApplication/ViewContrlollers/MyViewControllers/MyUITableView.h
@@ -0,0 +1,27 @@
+//
+//  MyUITableView.h  UITableView
+//  MyApplication
+//
+//  Created by Xiaofeng.Ge on 15/9/24.
+//  Copyright (c) 2015年 Gao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/*
+ TableView的数据源UITableViewDataSource
+ TableView的委托UITableViewDelegate
+ 如果当前类是继承自UIViewController，需要添加上面的代码，如果直接继承自UITableViewController则不需要添加
+ 
+ 
+ */
+
+/**
+ *  UITableView 学习
+ */
+@interface MyUITableView : UIViewController<UITableViewDataSource,UITableViewDelegate>
+
+@property(nonatomic,retain) NSArray *dataList;
+@property(nonatomic,retain) UITableView *tableView;
+
+@end
